e9c2e8e79f84bad533c1add5f04b6d99f3929493
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-09 23:51:48 +0000

    min

---
+++ src/fb2tex.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+'''
+FictionBook2 -> TeX converter
+
+Author: Vadim Zaliva <lord@crocodile.org>
+'''
+
+
+import getopt, sys
+
+import fictionbook
+
+def usage():
+    sys.stderr.write("Usage: fb2tex.py -f fb2file -o texfile\n")
+
+def main():
+
+    infile = None
+    outfile = None
+    verbose = False
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "vf:o:", ["verbose", "file", "output"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("-f", "--file"):
+            infile = a
+        if o in ("-o", "--output"):
+            outfile = a
+        if o in ("-v", "--verbose"):
+            verbose = True
+
+    if len(args) != 0 or infile is None or outfile is None:
+        usage()
+        sys.exit(2)
+
+    fb = fictionbook.Book()
+    fb.parseFile(infile)
+
+if __name__ == "__main__":
+    main()
