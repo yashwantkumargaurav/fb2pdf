a3a92b1fad84880a544647036fce32e636cf8ca1
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-10 03:27:44 +0000

    parsing title, athor, basic Latex generation framework. split parset into 2 classes importte google goopy 0.1 (functional.py)

--- trunk/src/fictionbook.py
+++ trunk/src/fictionbook.py
@@ -3,53 +3,66 @@ FictionBook2 module
 '''
 
 import sys, string
-from xml.sax import handler, parse
+import cbparser
 
-class Book(handler.ContentHandler):
+class TexFBook(cbparser.XMLCbParser):
 
+    def __init__(self, outfile):
+        cbparser.XMLCbParser.__init__(self)
+        self.outfile = outfile
+        self.author = None
+        self.title = None
+
+    def _uwrite(self, ustr):
+        self.f.write(ustr.encode('utf-8')) 
+                
     # callbacks
 
-    def start_FictionBook_body(self, attrs):
-        print "start_FictionBook"
+    def start_FictionBook_description_title__info_author(self, attrs):
+        self.author={}
 
-    def end_FictionBook_body(self):
-        print "end_FictionBook"
+    def chars_FictionBook_description_title__info_author_first__name(self, text):
+        self.author['first']=text
+        
+    def chars_FictionBook_description_title__info_author_middle__name(self, text):
+        self.author['middle']=text
+        
+    def chars_FictionBook_description_title__info_author_last__name(self, text):
+        self.author['last']=text
 
-    # Implementation
-
-    def __init__(self):
-        self.path = []
-
-    def parseFile(self, fname):
-        inFile = open(fname, 'r')
-        parse(inFile, self)
-        inFile.close()
-
-    def callHandler(self, *args):
-        prefix = args[0]
-        args = args[1:]
-        methodname = prefix + "_" + string.join(self.path,"_")
-        try:
-            method = getattr(self, methodname)
-            if len(args):
-                method(args)
+    def end_FictionBook_description_title__info_author(self):
+        self.author_name = u''
+        for k in ['first','middle','last']:
+            if self.author.has_key(k):
+                v = self.author[k]
             else:
-                method()
-        except AttributeError:
-            pass
-        
-    # ContentHadler callbacks
+                v = None
+            if v:
+                if len(self.author_name)==0:
+                    self.author_name = v
+                else:
+                    self.author_name = self.author_name + u' ' + v
+        self.f.write("\\author{")
+        self._uwrite(self.author_name)
+        self.f.write("}\n");
 
-    def startElement(self, name, attrs):
-        self.path.append(name)
-        self.callHandler("start", attrs)
-        
-    def endElement(self, name):
-        self.callHandler("end")
-        self.path.pop()
+    def chars_FictionBook_description_title__info_book__title(self, text):
+        self.f.write("\\title{")
+        self._uwrite(text)
+        self.f.write("}\n")
 
-    def characters( self, content):
-        pass
-        #self.callHandler("chars", content)
+    def start_FictionBook(self, attrs):
+        self.f = open(self.outfile,"w")
+        self.f.write("\\documentclass[11pt]{book}\n")
+        self.f.write("\\usepackage{graphicx}\n")
+        self.f.write("\\usepackage{url}\n")
+        self.f.write("\\usepackage{verbatim}\n")
+        self.f.write("\\usepackage[koi8-r]{inputenc}\n")
+        self.f.write("\\usepackage[russian]{babel}\n")
+        self.f.write("\\usepackage[papersize={9cm,12cm}, margin=4mm, ignoreall, pdftex]{geometry}\n")
+        self.f.write("\\begin{document}\n")
 
+    def end_FictionBook(self):
+        self.f.write("\\end{document}\n")
+        self.f.close()
 
