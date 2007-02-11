cd150f1ab93fb63d128899395878c25bc7ce0a2e
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-11 22:51:21 +0000

    switching to DOM (BeautifulSoup) based parser (work in progress)

--- src/fb2tex.py
+++ src/fb2tex.py
@@ -6,10 +6,150 @@ FictionBook2 -> TeX converter
 Author: Vadim Zaliva <lord@crocodile.org>
 '''
 
+import getopt, sys, string
 
-import getopt, sys
+from BeautifulSoup import BeautifulStoneSoup, Tag
 
-import fictionbook
+def _pQuote(str):
+    ''' Basic paragraph TeX quoting '''
+    if len(str)==0:
+        return str
+    # 'EN DASH' at the beginning of paragrapg - russian direct speach
+    if ord(str[0])==8211:
+        str="\\cdash--*" + str[1:]
+    return str
+
+def _text(t):
+    return string.join(t.contents)
+
+def _uwrite(f, ustr):
+    f.write(ustr.encode('utf-8')) 
+
+def fb2tex(infile, outfile):
+    f = open(infile, 'r')
+    soup = BeautifulStoneSoup(f)
+    f.close()
+
+    f = open(outfile, 'w')
+    f.write("\\documentclass[11pt]{book}\n")
+    f.write("\\usepackage{graphicx}\n")
+    f.write("\\usepackage{url}\n")
+    f.write("\\usepackage{epigraph}\n")
+    f.write("\\usepackage{verbatim}\n")
+    f.write("\\usepackage[utf-8]{inputenc}\n")
+    f.write("\\usepackage[russian]{babel}\n")
+    f.write("\\usepackage[papersize={9cm,12cm}, margin=4mm, ignoreall, pdftex]{geometry}\n")
+    f.write("\\setcounter{secnumdepth}{-2}\n"); # supress section numbering
+    f.write("\n\\begin{document}\n\n")
+
+    fb = soup.find("fictionbook")
+    processTitleAndAuthor(fb,f)
+    processSections(fb.find("body"), f)
+    
+    f.write("\\tableofcontents\n"); #TODO: move to beginning, make hyperlined index.
+    f.write("\n\\end{document}\n")
+    f.close()
+
+def processSections(b,f):
+    ss = b.findAll("section", recursive=False)
+    for s in ss:
+        processSection(s, f)
+
+def processSection(s, f):
+    t = s.find("title", recursive=False)
+    title = ""
+    if t:
+        for tx in t:
+            if isinstance(tx, Tag):
+                if tx.name == "p":
+                    if len(title):
+                        title = title + "\\\\" + _text(tx)
+                    else:
+                        title = _text(tx)
+                elif tx.name == "empty-line":
+                    if len(title):
+                        title = title + "\\\\"
+
+    # TODO: epigraphs
+    f.write("\\section{")
+    _uwrite(f,title) # TODO quote
+    f.write("}\n");
+                        
+    for x in s:
+        if isinstance(x, Tag):
+            if x.name == "section":
+                processSection(x,f)
+            if x.name == "p":
+                _uwrite(f,_pQuote(_text(x)))
+                f.write("\n\n")
+            elif x.name == "empty-line":
+                f.write("\n\n") # TODO: not sure
+
+def processTitleAndAuthor(fb,f):
+
+    #TODO: body/title
+    desc = fb.find("description")
+    if not desc:
+        return
+    ti = fb.find("title-info")
+    if not ti:
+        return
+    t = ti.find("book-title")
+    if t:
+        title = _text(t)
+    else:
+        title = ""
+    a = ti.find("author")
+    if not a:
+        author_name = ""
+    else:
+        fn = a.find("first-name")
+        if fn:
+            author_name = _text(fn)
+        else:
+            author_name = ""
+        mn = a.find("middle-name")
+        if mn:
+            if author_name:
+                author_name = author_name + " " + _text(mn)
+            else:
+                author_name = _text(mn)
+        ln = a.find("last-name")
+        if ln:
+            if author_name:
+                author_name = author_name + " " + _text(ln)
+            else:
+                author_name = _text(ln)
+                
+    if author_name:
+        f.write("\\author{")
+        _uwrite(f,author_name)
+        f.write("}\n");
+
+    if title:
+        f.write("\\title{")
+        _uwrite(f, title)
+        f.write("}\n")
+
+    if author_name or title:
+        f.write("\\maketitle\n");
+
+    # TODO: PDF info generation temporaty disabled. It seems that
+    # non ASCII characters are not allowed there!
+    if False and (author_name or title):
+        f.write("\n\\pdfinfo {\n")
+
+        if author_name:
+            f.write("\t/Title (")
+            _uwrite(f,author_name) #TODO quoting, at least brackets
+            f.write(")\n")
+
+        if title:
+            f.write("\t/Author (")
+            _uwrite(f,title) #TODO quoting, at least brackets
+            f.write(")\n")
+
+        f.write("}\n")
 
 def usage():
     sys.stderr.write("Usage: fb2tex.py -f fb2file -o texfile\n")
@@ -37,8 +177,7 @@ def main():
         usage()
         sys.exit(2)
 
-    fb = fictionbook.TexFBook(outfile)
-    fb.parseFile(infile)
+    fb2tex(infile, outfile)
 
 if __name__ == "__main__":
     main()
