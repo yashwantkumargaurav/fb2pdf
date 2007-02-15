22f91e8ee935c24d27e95bc4eff46f02afee2b3c
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-15 03:30:41 +0000

    parsing XML messages, checking versions

--- src/fbdaemon.py
+++ src/fbdaemon.py
@@ -7,12 +7,14 @@ Author: Vadim Zaliva <lord@crocodile.org>
 '''
 
 import getopt,sys,string,time
-
+from xml.dom.minidom import parse, parseString
 from ConfigParser import ConfigParser
 
 from boto.connection import SQSConnection
 from boto.sqs.message import Message
 from boto.exception import SQSError
+
+MSG_FORMAT_VER=1
 
 # --- Code ---
 
@@ -61,7 +63,41 @@ def main():
                     print "Error processing message"
 
 def processMessage(m):
-    print m.get_body()
+    msg = None
+    try:
+        msg = parseString(m.get_body())
+    except:
+        print "Invalid message, could not be parsed. Dropping"
+        return
+        
+    root = msg.childNodes[0]
+    if root.nodeName != 'fb2pdfjob':
+        print "Unknwon XML root element '%s'" % root.nodeName
+        return
+    v=root.getAttribute('version')
+    if not v or int(v)!=MSG_FORMAT_VER: 
+        print "Unsupported message format version '%s'" % v
+        return
+
+    srcs=root.getElementsByTagName('source')
+    if len(srcs)!=1:
+        print "Too many sources in the message!"
+        return
+    src = srcs[0]
+    src_url = src.getAttribute('url')
+    src_type = src.getAttribute('type')
+
+    results=root.getElementsByTagName('result')
+    if len(srcs)!=1:
+        print "Too many results in the message!"
+        return
+    res = results[0]
+    res_key = res.getAttribute('key')
+
+    print src_url
+    print src_type
+    print res_key
+    
 
 if __name__ == "__main__":
     main()
