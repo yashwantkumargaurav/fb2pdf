31fbb7b177288dd82255f5aa25d052ddefd8bd3b
vberko <vberko@f05116cd-5028-0410-9863-11db5debc633>
2007-02-20 06:19:31 +0000

    aws settings, s3 sqs classes

---
+++ www/awscfg.php
@@ -0,0 +1,14 @@
+<?php
+
+// amazon keys
+$awsApiKey = "";        // Specify key here
+$awsApiSecretKey = "";  // Specify secret key here
+
+// amazon s3 storage settings
+$awsS3Bucket = "fb2pdf";
+
+// amazon Simple Queue Service settings
+$awsSQSQueue = "fb2pdf";
+$awsSQSTimeout = 600;
+
+?>
