9bdf03b876d6b88dfebd6ea765d50426e0a6d815
vberko <vberko@f05116cd-5028-0410-9863-11db5debc633>
2007-02-21 04:44:55 +0000

    implementation of the amazon s3

--- trunk/www/aws.php
+++ trunk/www/aws.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'awscfg.php';
-require_once 's3.class.php';
+require_once 's3.php';
 
 // Put file to amazon s3
 function store_file($filePath)
@@ -8,17 +8,19 @@ function store_file($filePath)
     global $awsApiKey, $awsApiSecretKey, $awsS3Bucket;
 
     // S3
-    $s3 = new S3();
-    $s3->keyId = $awsApiKey;
-    $s3->secretKey = $awsApiSecretKey;
+    $s3 = new S3($awsApiKey, $awsApiSecretKey);
 
-    // create a bucket
-    $s3->putBucket($awsS3Bucket);
+    // create an object
+    $metadata = array("status"=>"", "result"=>"");
+    if (!$s3->createObject($awsS3Bucket, "test", "blah-blah", "text/html", "public-read", $metadata))
+        die ("Error creating object");
+        
+    $meta = $s3->getObjectMetadata($awsS3Bucket, "test");
+    print("Got methadata:<br>");
+    print_r($meta);
     
-    // create a file with metadata
     
     return TRUE;
 }
-    
-
+  
 ?>
