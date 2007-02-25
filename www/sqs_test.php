6b6f93d688b6328b7fa61356e8af2e72da938267
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-25 05:07:24 +0000

    SQS test utility

---
+++ www/sqs_test.php
@@ -0,0 +1,19 @@
+<?php
+
+  /**
+   * Command-line script to test SQS message sending
+   */
+
+require_once 'awscfg.php';
+require_once 'sqshelper.php';
+
+// send SQS message
+if(!sqsPutMessage('testid', "http://s3.amazonaws.com/test_bucker/test.fb2", 'test.fb2'))
+{
+    echo "Error sending message";
+} else
+{
+    echo "Message sent";
+}
+
+?>
