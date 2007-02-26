fa98bff90dce81b3223a55610d4c02098661be75
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-26 07:33:47 +0000

    cosmetics

--- trunk/www/sqs_test.php
+++ trunk/www/sqs_test.php
@@ -10,10 +10,10 @@ require_once 'sqshelper.php';
 // send SQS message
 if(!sqsPutMessage('testid', "http://s3.amazonaws.com/test_bucker/test.fb2", 'test.fb2'))
 {
-    echo "Error sending message";
+    echo "Error sending message\n";
 } else
 {
-    echo "Message sent";
+    echo "Message sent\n";
 }
 
 ?>
