72ecfeaa9071754353f180b867f041507c5dc7b3
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-02-28 00:46:12 +0000

    code reorg

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.4
+
+from distutils.core import setup
+
+setup(name='fb2pdf',
+      version='1.0',
+      description='FictionBook2 to PDF conversion tools',
+      author='Vadim Zaliva, et. al.',
+      author_email='lord@crocodile.org',
+      url='http://code.google.com/p/fb2pdf/',
+      packages=['fb2pdf'],
+      package_dir={'fb2pd': 'src/fb2pdf'},
+      package_data={'fb2pdf': ['src/TeX/*.sty']},
+     )
