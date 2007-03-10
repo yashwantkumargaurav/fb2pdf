846a7298cf34b08b0b0912851bc6f71c300f1f0d
vberko <vberko@f05116cd-5028-0410-9863-11db5debc633>
2007-03-10 01:52:51 +0000

    md5 added

--- src/sql/create_tables.sql
+++ src/sql/create_tables.sql
@@ -11,13 +11,18 @@ CREATE TABLE IF NOT EXISTS Books
         author      VARCHAR(128) NULL,
         title       VARCHAR(256) NULL,
         
+        -- md5 of the book's content
+        md5_hash    BIGINT NOT NULL,
+        
         -- conversion status ('r' - ready, 'e' - error)
         status      CHAR(1) DEFAULT NULL,
         
         INDEX   storage_key_idx (storage_key),
         INDEX   author_idx (author),
+        INDEX   md5_hash_idx (md5_hash),
         
         UNIQUE  storage_key_uniq (storage_key),
+        UNIQUE  md5_hash_uniq (md5_hash),
         
         PRIMARY KEY(id)
 ) TYPE=INNODB;
