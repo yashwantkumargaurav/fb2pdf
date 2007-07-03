4055b3f7c4d8b99680c34a81dc189a745908f777
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-07-03 06:47:26 +0000

    cosmetics

--- src/fb2pdf/fb2tex.py
+++ src/fb2pdf/fb2tex.py
@@ -217,21 +217,33 @@ def par(p, intitle=False):
             res += _textQuote(s.data)
     return res            
 
-def norec(an,defret,initial=[]):
+def norec(an=0,defret=None,initial=[]):
+    """ Recursion/loop prevention function decorator
+
+        For example, could be used when traversing a graph, which nodes assigned
+        unique IDs and you would like to avoid loops.
+
+        Keyword arguments:
+        an -- position of function argument, which is used as unique id (0-based)
+        defret -- default return value, which is returned when recursion is detected
+        initial -- initial content of call stack. Array of unique ids
+
+        Vadim Zaliva <lord@crocodile.org>
+    """
     class decorate:
         def __init__(self):
-            self.ns=initial
+            self.cs=initial
             
         def __call__(self, f):
             def new_f(*args, **kwds):
-                a0=args[an]
-                if a0 in self.ns:
-                    #print "recursion detected at '%s' in %s" % (a0, str(self.ns))
+                id=args[an]
+                if id in self.cs:
+                    #print "recursion detected at '%s' in %s" % (id, str(self.cs))
                     return defret
                 else:
-                    self.ns.append(a0)
+                    self.cs.append(id)
                     x = f(*args, **kwds)
-                    self.ns.remove(a0)
+                    self.cs.remove(id)
                     return x
             return new_f
     return decorate()
