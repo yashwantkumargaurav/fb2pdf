db01d76474cdadbdffa4a26771ab4bdeba7ed1d3
dennisberko <dennisberko@f05116cd-5028-0410-9863-11db5debc633>
2007-07-05 23:20:15 +0000

    generate_rss.php renamed to atom.php, Added ATOM date conversion function to utils.php

--- www/atom.php
+++ www/atom.php
@@ -1,13 +1,18 @@
 ﻿<?php
 	require_once "awscfg.php";
 	require_once "db.php";
-	//Normall I would indent and put propper spacing, but it doesn't work any other way
+	require_once "utils.php";
+	
+	$current_url = getFullUrl("atom.php");
+	//Normally I would indent and put propper spacing, but it doesn't work any other way
 	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <feed xmlns=\"http://www.w3.org/2005/Atom\">
+<link rel=\"self\" href=\"$current_url\"/> 
 
 	<title>Книги, сконвертированные недавно</title>
 	<link href=\"http://codeminders.com/fb2pdf/staging/\"/>
 	<id>urn:uuid:60a76c80</id>
+	<updated>".date("Y\-m\-d\TH\:i\:s\Z")."</updated>
 	";
 	$limit = (isset($_GET["limit"])) ? $_GET["limit"] : 15;
 	
@@ -19,13 +24,17 @@
 		$title   =    $list[$i]["title"];
 		$author  =    $list[$i]["author"];
 		$id      =    $list[$i]["id"];
+		$date    =    formatDateIntoAtom($list[$i]["converted"]);
 		$key     =    "getfile.php?key=" . $list[$i]["storage_key"]."";
 		echo "
 		<entry>
 			<title>$title</title>
 			<link href=\"$key\"/>
-			<author>$author</author>
+			<author>
+				<name>$author</name>
+			</author>
 			<id>urn:uuid:$id</id>
+			<updated>$date</updated>
 			<summary>$author</summary>
 		</entry>
 		";
