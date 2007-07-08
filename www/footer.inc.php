b43f965c9636520448dc283720a0cf7d30f4dd01
dennisberko <dennisberko@f05116cd-5028-0410-9863-11db5debc633>
2007-07-08 06:28:22 +0000

    ATOM image on the bottom Removed ATOM by author link from books.php until we figure out how to do this for all authors

--- www/footer.inc.php
+++ www/footer.inc.php
@@ -1,4 +1,17 @@
+<?php
+	/*
+	$author  =  $_GET["author"];
+	$base    =  "atom.php";
+	$link    =  (isset($author)) ? "$base?author=$author" : $base;
+	*/
+	$link = "atom.php";
+?>
 <p class="end_line">Обнаружили ошибку? У Вас есть предложения по улучшению сервиса? Хотите оставить комментарий?
 <br/>Это можно сделать <a href="http://groups.google.com/group/fb2pdf-users/about?hl=ru">здесь</a>
 <div></div>
-<a href="http://www.crocodile.org/"><img src="images/noir.png" class="left" width="80" height="15"></a>
+<?php
+echo "
+<a href=\"$link\">
+";
+?>
+<img src="images/atom.gif"/></a>&nbsp;&nbsp;<a href="http://www.crocodile.org/"><img src="images/noir.png" class="left" width="80" height="15"></a>
