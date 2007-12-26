d24c99317e10fa253d62d4abcf6c228e53fc5bcd
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2007-12-26 03:55:59 +0000

    Basic merging work. need work on titles and sections.

--- trunk/src/fbmerge/r.sh
+++ trunk/src/fbmerge/r.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-rm -f kuprin.fb2 
-./fbmerge.py -v -o kuprin.fb2 ~/tmp/fb2mergedata/*xml
+rm -f ~/tmp/fb2mergedata/kuprin.fb2 ~/tmp/fb2mergedata/kuprin.tex ~/tmp/fb2mergedata/kuprin.pdf
+./fbmerge.py -v -o ~/tmp/fb2mergedata/kuprin.fb2 ~/tmp/fb2mergedata/*xml ~/tmp/fb2mergedata/*fb2
+
+fb2tex -v -f ~/tmp/fb2mergedata/kuprin.fb2 -o ~/tmp/fb2mergedata/kuprin.tex
+(cd ~/tmp/fb2mergedata/; pdflatex kuprin.tex; pdflatex kuprin.tex)
+open ~/tmp/fb2mergedata/kuprin.pdf
