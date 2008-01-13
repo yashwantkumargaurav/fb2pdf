0b65d4f0f90a53d8f098193da0603ed33d2395d3
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2008-01-13 07:35:43 +0000

    moved fbmerge to scripts

--- src/fbmerge/r.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -f ~/tmp/fb2mergedata/kuprin.fb2 ~/tmp/fb2mergedata/kuprin.tex ~/tmp/fb2mergedata/kuprin.pdf
-./fbmerge.py -v -o ~/tmp/fb2mergedata/kuprin.fb2 ~/tmp/fb2mergedata/*xml ~/tmp/fb2mergedata/*fb2
-
-fb2tex -v -f ~/tmp/fb2mergedata/kuprin.fb2 -o ~/tmp/fb2mergedata/kuprin.tex -m anthology
-(cd ~/tmp/fb2mergedata/; pdflatex kuprin.tex; pdflatex kuprin.tex)
-open ~/tmp/fb2mergedata/kuprin.pdf
