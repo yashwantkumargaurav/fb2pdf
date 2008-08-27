e46b8b0cbaf66afa8bb28c7cbd0b7e3fcd4c2e9d
vberko <vberko@f05116cd-5028-0410-9863-11db5debc633>
2008-08-27 23:35:34 +0000

    Support new version of the SQS

--- trunk/www/sqshelper.php
+++ trunk/www/sqshelper.php
@@ -1,79 +1,35 @@
 <?php
 require_once 'awscfg.php';
-require_once 'sqs.php';
+require_once 'sqs.client.php';
 
 function sqsPutMessage($id, $sourceUrl, $name, $callbackUrl, $callbackPassword, $email)
 {
     global $awsApiKey, $awsApiSecretKey, $awsSQSQueue, $awsSQSTimeout;
 
-    $sqs = new SQS($awsApiKey, $awsApiSecretKey);
-    if (!$sqs->createQueue($awsSQSQueue))
+    $sqs = new SQSClient($awsApiKey, $awsApiSecretKey, 'http://queue.amazonaws.com');
+    
+	try
     {
-        error_log("FB2PDF ERROR. sqshelper: " . $sqs->responseString); 
+        // Create the queue.  TODO: If the queue has recently been deleted, the application needs to wait for 60 seconds before
+        $sqs->CreateQueue($awsSQSQueue);
+        
+        // Send a message to the queue
+        $message = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><fb2pdfjob version=\"3\">" . 
+            "<source url=\"$sourceUrl\" type=\"application/fb2+xml\" name=\"$name\"/>" .
+            "<result key=\"$id.zip\" encoding=\"application/zip\"/>" .
+            "<log key=\"$id.txt\"/>" .
+            "<callback url=\"$callbackUrl\" method=\"POST\" params=\"pass=$callbackPassword&amp;email=$email\"/>" .
+            "</fb2pdfjob>";
+
+        $sqs->SendMessage(base64_encode($message));
+    }
+    catch(Exception $e)
+    {
+        $err_str = $e->getMessage();
+        error_log("FB2PDF ERROR. sqshelper: " . $err_str); 
         return false;
     }
-
-    $queueUrl = parseCreateQueueResponse($sqs->responseString);
-    $queueUrl = str_replace("http://queue.amazonaws.com/","",$queueUrl);
-
-    $message = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><fb2pdfjob version=\"3\">" . 
-        "<source url=\"$sourceUrl\" type=\"application/fb2+xml\" name=\"$name\"/>" .
-        "<result key=\"$id.zip\" encoding=\"application/zip\"/>" .
-        "<log key=\"$id.txt\"/>" .
-        "<callback url=\"$callbackUrl\" method=\"POST\" params=\"pass=$callbackPassword&amp;email=$email\"/>" .
-        "</fb2pdfjob>";
-
-    if (!$sqs->putMessage(base64_encode($message), $queueUrl, $awsSQSTimeout))
-    {
-        error_log("FB2PDF ERROR. sqshelper: " . $sqs->responseString); 
-        return false;
-    }
-
+    
     return true;
-}
-
-$sqsProcessQueueUrl=false;
-$sqsQueueUrl=null;
-
-function parseCreateQueueResponse($response)
-{
-    global $sqsQueueUrl;
-    
-    // Initialize the XML parser
-    $parser=xml_parser_create();
-    xml_set_element_handler($parser,"sqs_element_start","sqs_element_end");
-    xml_set_character_data_handler($parser,"sqs_element_data");    
-    
-    $error = false;
-    if (!xml_parse($parser,$response,true))
-        $error = true;
-    
-    xml_parser_free($parser);
-    return (!$error) ? $sqsQueueUrl : null;
-}
-// Function to use at the start of an element
-function sqs_element_start($parser,$element_name,$element_attrs)
-{
-    global $sqsProcessQueueUrl;
-    
-    if ($element_name == "QUEUEURL")
-        $sqsProcessQueueUrl = true;
-}
-
-// Function to use at the end of an element
-function sqs_element_end($parser,$element_name)
-{
-    global $sqsProcessQueueUrl;
-    
-    if ($element_name == "QUEUEURL")
-        $sqsProcessQueueUrl = false;
-}
-
-function sqs_element_data($parser,$data)
-{
-    global $sqsProcessQueueUrl, $sqsQueueUrl;
-    
-    if ($sqsProcessQueueUrl)
-        $sqsQueueUrl = $data;
 }
 ?>
