af7aa00c07b5bea6de32ac7464d1db09e3bc20a1
vberko <vberko@f05116cd-5028-0410-9863-11db5debc633>
2008-12-24 08:29:26 +0000

    fix green line. Add a title to the rating

--- www/book.php
+++ www/book.php
@@ -58,7 +58,7 @@ catch(Exception $e)
     <div id="tab_box">
         <b class="xtop"><b class="xb1"></b><b class="xb2"></b><b class="xb3"></b><b class="xb4"></b></b>
         <div class="tab_box_content">
-            <img src="images/lg_px.gif" class="line"/>
+            <img src="images/green_px.gif" class="line"/>
             <div id="status" class="message">
                 <?php        
                 echo "<h3 class='left'><a href='books.php?author=$author' style='color:black'>$author</a>&nbsp;&nbsp;\"$title\"</h3><br/><br/>";
@@ -66,15 +66,16 @@ catch(Exception $e)
                 echo "[<a href='$bs->pdfFile'>Sony Reader (pdf)</a>]&nbsp;&nbsp;[<a href='$bs->fbFile'>оригинал (fb2)</a>]</p>";
                 echo "<p><a href='books.php?author=$author' style='color:black'>Другие книги автора</a></p>";
                 echo "<p>";
-                $book_link="/book.php?key=".urlencode($key);
-                $book_path="/book/".$key;
-                echo "<div class=\"js-kit-rating\" permalink=\"$book_link\" path=\"$book_path\"></div>";
+                $book_link  = "/book.php?key=".urlencode($key);
+                $book_path  = "/book/".$key;
+                $book_title = $author."  ".$title;
+                echo "<div class=\"js-kit-rating\" title=\"$book_title\" permalink=\"$book_link\" path=\"$book_path\"></div>";
                 echo "<div class=\"js-kit-comments\" permalink=\"$book_link\" path=\"$book_path\"></div>";
                 echo "<script src=\"http://js-kit.com/reviews.js\" permalink=\"$book_link\" path=\"$book_path\"></script>";
                 ?>
                 
             </div>    
-     <img src="images/lg_px.gif" class="line"/>
+            <img src="images/green_px.gif" class="line"/>
             <?php include 'footer.inc.php'; ?>
         </div>  <!--end of tab box content-->	
         <b class="xbottom"><b class="xb4"></b><b class="xb3"></b><b class="xb2"></b><b class="xb1"></b></b>
