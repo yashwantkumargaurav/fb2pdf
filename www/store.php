d87096829357b147bc1d2fb6dc5528054832184b
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2009-02-09 22:14:13 +0000

    link to new kindle

--- www/store.php
+++ www/store.php
@@ -46,6 +46,9 @@
 
 <iframe src="http://rcm.amazon.com/e/cm?t=wwwcrocodilorg&o=1&p=8&l=as1&asins=B001AFTWTE&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 
+<iframe src="http://rcm.amazon.com/e/cm?t=wwwcrocodilorg&o=1&p=8&l=as1&asins=B00154JDAI&md=10FE9736YVPPT7A0FBG2&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+
+
 
 </p>
             <img src="images/green_px.gif" class="line"/>
