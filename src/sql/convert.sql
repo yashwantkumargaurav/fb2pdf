0ae450c75756ce1d1060f73ac36fb25453477d93
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2009-04-19 04:03:11 +0000

    DB conversion script (tested on real data)

---
+++ src/sql/convert.sql
@@ -0,0 +1,10 @@
+-- SQL script to convert from old to new db schema
+
+DELETE FROM OriginalBooks;
+DELETE FROM ConvertedBooks;
+
+INSERT INTO OriginalBooks (id, storage_key, author, title, isbn, md5hash)
+       SELECT id, storage_key, author, title, isbn, md5hash FROM Books;
+
+INSERT INTO ConvertedBooks (book_id, format, status, converted, counter, conv_ver)
+       SELECT id, 1, status, converted, counter, conv_ver FROM Books;
