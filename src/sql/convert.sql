4afa7a1029426a5d7dfc12b27b8460be963e06c8
krokodil <krokodil@f05116cd-5028-0410-9863-11db5debc633>
2009-04-21 02:33:43 +0000

    set valid status on conversion

--- src/sql/convert.sql
+++ src/sql/convert.sql
@@ -4,7 +4,7 @@ DELETE FROM OriginalBooks;
 DELETE FROM ConvertedBooks;
 
 INSERT INTO OriginalBooks (id, storage_key, author, title, isbn, md5hash, submitted, valid)
-       SELECT id, storage_key, author, title, isbn, md5hash, converted, FALSE FROM Books;
+       SELECT id, storage_key, author, title, isbn, md5hash, converted, status="r" FROM Books;
 
 INSERT INTO ConvertedBooks (book_id, format, status, converted, counter, conv_ver)
        SELECT id, 1, status, converted, counter, conv_ver FROM Books;
