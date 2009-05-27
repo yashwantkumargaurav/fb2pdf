c9f39ed916a88621ddc140073364015316591237
zlatkarp <zlatkarp@f05116cd-5028-0410-9863-11db5debc633>
2009-05-27 07:33:24 +0000

    Split DB and process functions for book and format.

--- www/getfile.php
+++ www/getfile.php
@@ -12,13 +12,14 @@ if (!isset ($_GET["key"]))
 else
 {
     $key = $_GET["key"];
+    $format = $_GET["format"];
     $url = "http://s3.amazonaws.com/$awsS3Bucket/$key";
 
     // update counter in the DB
     if (strstr($key, ".zip") or strstr($key, ".pdf"))
     {
         $db = getDBObject();
-        if (!$db->updateBookCounter($key))
+        if (!$db->updateBookCounter($key, $format))
             error_log("FB2PDF ERROR. Unable to update book's counter. Key=$key"); 
     }
     
