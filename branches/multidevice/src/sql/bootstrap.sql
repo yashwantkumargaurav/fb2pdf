a1971a7fc94788f363179bf191ed33abc569acb4
zlatkarp <zlatkarp@f05116cd-5028-0410-9863-11db5debc633>
2009-11-03 15:38:46 +0000

    Update format paramters for Jetbook

--- branches/multidevice/src/sql/bootstrap.sql
+++ branches/multidevice/src/sql/bootstrap.sql
@@ -14,12 +14,12 @@ INSERT INTO FormatParams (format, name, value) VALUES (2, 'papersize','122.4mm,9
 INSERT INTO Formats (id, title, description) VALUES (3, 'ECTACO JetBook', 'ECTACO JetBook (portrait)');
 INSERT INTO FormatParams (format, name, value) VALUES (3, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (3, 'fontsize','12pt');
-INSERT INTO FormatParams (format, name, value) VALUES (3, 'papersize','79mm,105mm');
+INSERT INTO FormatParams (format, name, value) VALUES (3, 'papersize','79mm,100mm');
 
 INSERT INTO Formats (id, title, description) VALUES (4, 'ECTACO JetBook (landscape)', 'ECTACO JetBook (landscape)');
 INSERT INTO FormatParams (format, name, value) VALUES (4, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (4, 'fontsize','12pt');
-INSERT INTO FormatParams (format, name, value) VALUES (4, 'papersize','105mm,79mm');
+INSERT INTO FormatParams (format, name, value) VALUES (4, 'papersize','100mm,79mm');
 
 INSERT INTO Formats (id, title, description) VALUES (5, 'iPhone', 'Apple iPhone (portrait). Includes: iPhone, iPhone3G');
 INSERT INTO FormatParams (format, name, value) VALUES (5, 'margin','1mm');
