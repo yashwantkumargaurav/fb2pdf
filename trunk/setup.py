b612af65bfa37627ae2fc95c771c654add6cdb14
zlatkarp <zlatkarp@f05116cd-5028-0410-9863-11db5debc633>
2010-04-26 07:13:16 +0000

    Install epubgen-0.5.0.jar to /usr/share/java

--- trunk/setup.py
+++ trunk/setup.py
@@ -11,5 +11,5 @@ setup(name='fb2pdf',
       packages=['fb2pdf'],
       package_dir={'fb2pdf': 'src/fb2pdf'},
       scripts=['scripts/fbmerge','scripts/fb2pdf_queue_send','scripts/fb2pdf_queue_clear','scripts/fb2pdf_queue_receive','scripts/fbdaemon','scripts/fb2tex'],
-      data_files=[('share/texmf-local/', ['src/TeX/verse.sty', 'src/TeX/epigraph.sty','src/TeX/sectsty.dtx', 'src/TeX/sectsty.ins']),('share/fb2pdf',['etc/broken-image.png'])]
+      data_files=[('share/texmf-local/', ['src/TeX/verse.sty', 'src/TeX/epigraph.sty','src/TeX/sectsty.dtx', 'src/TeX/sectsty.ins']),('share/fb2pdf',['etc/broken-image.png']),('share/java',['lib/epubgen-0.5.0.jar'])]
      )
