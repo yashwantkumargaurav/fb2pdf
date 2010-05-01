7d123ace51f0e801ee185698a11075d8c29f584a
zlatkarp <zlatkarp@f05116cd-5028-0410-9863-11db5debc633>
2010-05-01 19:20:26 +0000

    Add support for coversion options for EPUB format. Register another EPUB format with transliterated title and list of contents. Install epubgen JAR file into /usr/share/fb2pdf

--- src/sql/bootstrap.sql
+++ src/sql/bootstrap.sql
@@ -1,84 +1,114 @@
 -- Bootstapping SQL
 -- Defines basic device formats
 
-INSERT INTO Formats (id, title, description) VALUES (1, 'Sony Reader PRS-500,505,600,700', 'Sony Reader (portrait). Includes: PRS-500,505,600,700');
+SET FOREIGN_KEY_CHECKS=0;
+BEGIN;
+
+DELETE FROM FormatParams;
+DELETE FROM Formats;
+
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (1, 'Sony Reader PRS-500,505,600,700', 'Sony Reader (portrait). Includes: PRS-500,505,600,700', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (1, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (1, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (1, 'papersize','90.6mm,122.4mm');
 
-INSERT INTO Formats (id, title, description) VALUES (2, 'Sony Reader PRS-500,505,600,700 (landscape)', 'Sony Reader (landscape). Includes: PRS-500,505,600,700');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (2, 'Sony Reader PRS-500,505,600,700 (landscape)', 'Sony Reader (landscape). Includes: PRS-500,505,600,700', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (2, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (2, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (2, 'papersize','122.4mm,90.6mm');
 
-INSERT INTO Formats (id, title, description) VALUES (3, 'ECTACO JetBook', 'ECTACO JetBook (portrait)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (3, 'ECTACO JetBook', 'ECTACO JetBook (portrait)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (3, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (3, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (3, 'papersize','79mm,100mm');
 
-INSERT INTO Formats (id, title, description) VALUES (4, 'ECTACO JetBook (landscape)', 'ECTACO JetBook (landscape)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (4, 'ECTACO JetBook (landscape)', 'ECTACO JetBook (landscape)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (4, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (4, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (4, 'papersize','100mm,79mm');
 
-INSERT INTO Formats (id, title, description) VALUES (5, 'iPhone', 'Apple iPhone (portrait). Includes: iPhone, iPhone3G');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (5, 'iPhone', 'Apple iPhone (portrait). Includes: iPhone, iPhone3G', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (5, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (5, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (5, 'papersize','80mm,120mm');
 
-INSERT INTO Formats (id, title, description) VALUES (6, 'iPhone (landscape)', 'Apple iPhone (landscape). Includes: iPhone, iPhone3G');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (6, 'iPhone (landscape)', 'Apple iPhone (landscape). Includes: iPhone, iPhone3G', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (6, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (6, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (6, 'papersize','120mm,80mm');
 
-INSERT INTO Formats (id, title, description) VALUES (7, 'Kindle DX', 'Amazon Kindle DX (portrate)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (7, 'Kindle DX', 'Amazon Kindle DX (portrate)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (7, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (7, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (7, 'papersize','132.8mm,194.8mm');
 
-INSERT INTO Formats (id, title, description) VALUES (8, 'Kindle DX (landscape)', 'Amazon Kindle DX (landscape)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (8, 'Kindle DX (landscape)', 'Amazon Kindle DX (landscape)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (8, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (8, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (8, 'papersize','194.8mm,132.8mm');
 
-INSERT INTO Formats (id, title, description) VALUES (9, 'Kindle 2', 'Amazon Kindle 2 (portrait)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (9, 'Kindle 2', 'Amazon Kindle 2 (portrait)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (9, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (9, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (9, 'papersize','90.6mm,122.4mm');
 
-INSERT INTO Formats (id, title, description) VALUES (10, 'Kindle 2 (landscape)', 'Amazon Kindle 2 (landscape)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (10, 'Kindle 2 (landscape)', 'Amazon Kindle 2 (landscape)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (10, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (10, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (10, 'papersize','122.4mm,90.6mm');
 
-INSERT INTO Formats (id, title, description) VALUES (11, 'iPad', 'Apple iPad (portrait)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (11, 'iPad', 'Apple iPad (portrait)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (11, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (11, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (11, 'papersize','147.8mm,197.1mm');
 
-INSERT INTO Formats (id, title, description) VALUES (12, 'iPad (landscape)', 'Apple iPad (landscape)');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (12, 'iPad (landscape)', 'Apple iPad (landscape)', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (12, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (12, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (12, 'papersize','197.1mm,147.8mm');
 
-INSERT INTO Formats (id, title, description) VALUES (13, 'Sony Reader PRS-300', 'Sony Reader (portrait). Includes: PRS-300');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (13, 'Sony Reader PRS-300', 'Sony Reader (portrait). Includes: PRS-300', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (13, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (13, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (13, 'papersize','76.12mm,101.5mm');
 
-INSERT INTO Formats (id, title, description) VALUES (14,  'Sony Reader PRS-300 (landscape)', 'Sony Reader (landscape). Includes: PRS-300');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (14,  'Sony Reader PRS-300 (landscape)', 'Sony Reader (landscape). Includes: PRS-300', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (14, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (14, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (14, 'papersize','101.5mm,76.12mm');
 
-INSERT INTO Formats (id, title, description) VALUES (15, 'Sony Reader PRS-900', 'Sony Reader (portrait). Includes: PRS-900');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (15, 'Sony Reader PRS-900', 'Sony Reader (portrait). Includes: PRS-900', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (15, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (15, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (15, 'papersize','91.17mm,155.6mm');
 
-INSERT INTO Formats (id, title, description) VALUES (16,  'Sony Reader PRS-900 (landscape)', 'Sony Reader (landscape). Includes: PRS-900');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (16,  'Sony Reader PRS-900 (landscape)', 'Sony Reader (landscape). Includes: PRS-900', 'pdf', 'zip');
 INSERT INTO FormatParams (format, name, value) VALUES (16, 'margin','1mm');
 INSERT INTO FormatParams (format, name, value) VALUES (16, 'fontsize','12pt');
 INSERT INTO FormatParams (format, name, value) VALUES (16, 'papersize','155.6mm,91.17mm');
 
-INSERT INTO Formats (id, filetype, title, description) VALUES (17,  'epub', 'EPUB', 'Open e-book standard by the International Digital Publishing Forum');
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (17,  'EPUB', 'Open e-book standard by the International Digital Publishing Forum', 'epub', 'none');
+INSERT INTO FormatParams (format, name, value) VALUES (17, '-translit','0');
+
+INSERT INTO Formats (id, title, description, filetype, compress) 
+       VALUES (18,  'EPUB (transliterated)', 'Open e-book standard by the International Digital Publishing Forum. Title and list of contents have been transliterated', 'epub', 'none');
+
+COMMIT;
+SET FOREIGN_KEY_CHECKS=1;
